f27343e1fba7c338f14ec651ac0651a41e9ec89d
J-P Nurmi <jpnurmi@gmail.com>
2025-03-18 11:19:57 +0100

    Drop SENTRY_PLAYGROUND_HANDLER on Windows

--- main.cpp
+++ main.cpp
@@ -5,7 +5,9 @@
 int main(int argc, char *argv[])
 {
     sentry_options_t *options = sentry_options_new();
+#ifdef SENTRY_PLAYGROUND_HANDLER
     sentry_options_set_handler_path(options, SENTRY_PLAYGROUND_HANDLER);
+#endif
     sentry_options_set_dsn(options, SENTRY_PLAYGROUND_DSN);
     sentry_options_set_release(options, SENTRY_PLAYGROUND_RELEASE);
     sentry_options_set_debug(options, 1);
