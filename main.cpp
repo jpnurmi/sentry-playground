fb8eed3f9b263e7a6dd342a98a82d98676105db8
J-P Nurmi <jpnurmi@gmail.com>
2025-07-01 11:07:30 +0200

    Revise SENTRY_XXX vars

--- main.cpp
+++ main.cpp
@@ -8,11 +8,9 @@ int main(int argc, char *argv[])
     SentryPlayground::debug().nospace() << "backend=" << SENTRY_BACKEND;
 
     sentry_options_t *options = sentry_options_new();
-#ifdef SENTRY_PLAYGROUND_HANDLER
-    sentry_options_set_handler_path(options, SENTRY_PLAYGROUND_HANDLER);
-#endif
-    sentry_options_set_dsn(options, SENTRY_PLAYGROUND_DSN);
-    sentry_options_set_release(options, SENTRY_PLAYGROUND_RELEASE);
+    sentry_options_set_dsn(options, SENTRY_DSN);
+    sentry_options_set_release(options, SENTRY_RELEASE);
+    sentry_options_set_handler_path(options, SENTRY_HANDLER_PATH);
     sentry_options_set_attach_screenshot(options, true);
     sentry_options_set_debug(options, 1);
     sentry_init(options);
