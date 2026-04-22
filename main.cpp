4f622e935276b37f2c6add64326e90ec5bfb924f
J-P Nurmi <jpnurmi@gmail.com>
2026-04-22 15:18:45 +0200

    playground environment

--- main.cpp
+++ main.cpp
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
     sentry_options_t *options = sentry_options_new();
     sentry_options_set_dsn(options, SENTRY_DSN);
     sentry_options_set_release(options, SENTRY_RELEASE);
-    sentry_options_set_environment(options, "development");
+    sentry_options_set_environment(options, "playground");
     sentry_options_set_handler_path(options, SENTRY_HANDLER_PATH);
     sentry_options_set_attach_screenshot(options, true);
     sentry_options_set_before_send(options, before_send, NULL);
